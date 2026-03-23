085e9d85d15581418d801ed2b1642ee1c8df5cc5
Brandon Roehl <me@brandonroehl.org>
2026-03-23 15:35:39 -0500

    posts

---
+++ app/posts/get-posts.ts
@@ -0,0 +1,23 @@
+import { Item, normalizePages } from 'nextra/normalize-pages'
+import { getPageMap } from 'nextra/page-map'
+
+export async function getPosts(): Promise<Item[]> {
+    const { directories } = normalizePages({
+        list: await getPageMap('/posts'),
+        route: '/posts'
+    })
+    return directories
+        .filter(post => post.name !== 'index')
+        .sort(
+            (a, b) => (
+                new Date(b.frontMatter.date).getTime()
+                - new Date(a.frontMatter.date).getTime()
+            )
+        )
+}
+
+export async function getTags(): Promise<string[]> {
+    const posts = await getPosts()
+    const tags = posts.flatMap(post => post.frontMatter.tags)
+    return tags
+}
