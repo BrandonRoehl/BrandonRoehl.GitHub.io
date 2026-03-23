ad64fc221de1b1662b3024440178d0971aecbef7
Brandon Roehl <me@brandonroehl.org>
2026-03-23 12:25:28 -0500

    kinda building

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import nextra from 'nextra'
+import type { NextConfig } from "next";
+
+// Set up Nextra with its configuration
+const withNextra = nextra({
+    // ... Add Nextra-specific options here
+    defaultShowCopyCode: true,
+    readingTime: true
+})
+
+const nextConfig: NextConfig = {
+    /* config options here */
+    reactStrictMode: true,
+    cleanDistDir: true,
+    turbopack: {
+        resolveAlias: {
+            // Path to your `mdx-components` file with extension
+            'next-mdx-import-source-file': './mdx-components.tsx'
+        }
+    }
+};
+
+export default withNextra(nextConfig)
+
